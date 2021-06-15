f411e8d7341dc77c68178abddc4c2f763e7a655e
Florin Rotaru <i.florin.rotaru@outlook.com>
2021-06-15 07:38:20 +0300

    updates

--- net-mappers-benchmarks/Program.cs
+++ net-mappers-benchmarks/Program.cs
@@ -26,30 +26,30 @@ namespace NetMappers.Benchmarks
             //.Create(DefaultConfig.Instance)
             //.WithOptions(ConfigOptions.DisableOptimizationsValidator);
 
-            //BenchmarkRunner.Run<From_Account_To_AccountDto>();
+            BenchmarkRunner.Run<From_Account_To_AccountDto>();
 
-            //BenchmarkRunner.Run<From_TC0_Members_To_TC0_I0_Members>();
+            BenchmarkRunner.Run<From_TC0_Members_To_TC0_I0_Members>();
 
-            //BenchmarkRunner.Run<From_TC0_Members_To_TC0_I1_Members>();
-            //BenchmarkRunner.Run<From_TC0_Members_To_TC0_I2_Nullable_Members>();
+            BenchmarkRunner.Run<From_TC0_Members_To_TC0_I1_Members>();
+            BenchmarkRunner.Run<From_TC0_Members_To_TC0_I2_Nullable_Members>();
 
-            //BenchmarkRunner.Run<From_TC0_Members_To_TS0_I0_Members>();
-            //BenchmarkRunner.Run<From_TC0_Members_To_TS0_I1_Members>();
-            //BenchmarkRunner.Run<From_TC0_Members_To_TS0_I2_Nullable_Members>();
+            BenchmarkRunner.Run<From_TC0_Members_To_TS0_I0_Members>();
+            BenchmarkRunner.Run<From_TC0_Members_To_TS0_I1_Members>();
+            BenchmarkRunner.Run<From_TC0_Members_To_TS0_I2_Nullable_Members>();
 
-            //BenchmarkRunner.Run<From_TS0_Members_To_TC0_I0_Members>();
-            //BenchmarkRunner.Run<From_TS0_Members_To_TC0_I1_Members>();
-            //BenchmarkRunner.Run<From_TS0_Members_To_TC0_I2_Nullable_Members>();
+            BenchmarkRunner.Run<From_TS0_Members_To_TC0_I0_Members>();
+            BenchmarkRunner.Run<From_TS0_Members_To_TC0_I1_Members>();
+            BenchmarkRunner.Run<From_TS0_Members_To_TC0_I2_Nullable_Members>();
 
-            //BenchmarkRunner.Run<From_TS0_Members_To_TS0_I0_Members>();
-            //BenchmarkRunner.Run<From_TS0_Members_To_TS0_I1_Members>();
-            //BenchmarkRunner.Run<From_TS0_Members_To_TS0_I2_Nullable_Members>();
+            BenchmarkRunner.Run<From_TS0_Members_To_TS0_I0_Members>();
+            BenchmarkRunner.Run<From_TS0_Members_To_TS0_I1_Members>();
+            BenchmarkRunner.Run<From_TS0_Members_To_TS0_I2_Nullable_Members>();
 
-            //BenchmarkRunner.Run<From_TC1_To_TC1_0>();
-            //BenchmarkRunner.Run<From_TC1_To_TS1_0>();
+            BenchmarkRunner.Run<From_TC1_To_TC1_0>();
+            BenchmarkRunner.Run<From_TC1_To_TS1_0>();
 
-            //BenchmarkRunner.Run<From_TS1_To_TC1_0>();
-            //BenchmarkRunner.Run<From_TS1_To_TS1_0>();
+            BenchmarkRunner.Run<From_TS1_To_TC1_0>();
+            BenchmarkRunner.Run<From_TS1_To_TS1_0>();
 
             var summaryPath = args.Length == 2 ? args[1] : string.Empty;
 
